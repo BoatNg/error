5cb1ebcc6b2513189a6746bb6219a40316d0527e
吴波(东风日产数据营销公司平台研发中心前端开发室) <wubo-df@dfl.com.cn>
2017-09-06 18:25:32 +0800

    add pm2

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-/*const mysql = require('mysql2');
+
 module.exports = app => {
-    app.beforeStart(function*() {
-        yield app.model.async({force: true});
-    })
-}*/
-/*
-// get the client 
-const mysql = require('mysql2');
- 
-// create the connection to database 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: '123456',
-  database: 'test'
-});
-// simple query 
-// connection.query(
-//   'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-//   function(err, results, fields) {
-//       console.log(err)
-//     console.log(results); // results contains rows returned by server 
-//     console.log(fields); // fields contains extra meta data about results, if available 
-//   }
-// );
-*/
+    console.log("*****==============================*****")
+}
